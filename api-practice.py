8d2429e3522f9dfa4e373b84ae5552cc03e3ef50
Kim Jin-yeong <yeong4625@gmail.com>
2021-04-06 12:42:09 +0900

    Initial commit

--- api-practice.py
+++ api-practice.py
@@ -2,11 +2,14 @@ import urllib.request as request
 import urllib.parse
 import requests as r
 
-params = urllib.parse.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
-print(params)
+# params = urllib.parse.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
+# print(params)
+#
+# url = f"http://www.musi-cal.com/cgi-bin/query?{params}"
+# print(url)
+#
+# response=request.urlopen(url)
+# print(response)
 
-url = f"http://www.musi-cal.com/cgi-bin/query?{params}"
-print(url)
 
-response=request.urlopen(url)
-print(response)
+
